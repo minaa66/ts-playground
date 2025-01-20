a194ae6efc92dfc6b97ee2c89b462adcbbd476fb
mina <imina3951@gmail.com>
2025-01-20 18:30:28 +0200

    parameters type in functions

--- concepts/basic-types.ts
+++ concepts/basic-types.ts
@@ -1,30 +1,51 @@
-//first program
-type Person = {
-    name:string
+// //first program
+// type Person = {
+//     name:string
+// }
+// const person:Person = {
+//     name:'john'
+// }
+// console.log(person.name)
+
+
+// //Strings
+// let myName:string= 'mina isaac'
+// // numbers 
+// let Fsvnumber: number = 9
+// //boolean
+// let flag:boolean= true
+
+
+// //type inference
+// let car = 'mersycide'
+// let favnumber = 9
+// //you can't assign it to different type now
+// //favnumber = 'mina'  this errors 
+
+
+
+// //any type
+// let color:any = 'green'
+// color = 20 //no error happens
+// //color();  // this will error only if you run code
+
+
+//function parameters  
+//normal function
+function AddOne (half:number , anotherHalf:number){
+    return half+anotherHalf
 }
-const person:Person = {
-    name:'john'
+console.log(AddOne(3,3));  // prints 6 
+
+//arrow function
+const double= (x:number) =>{
+    return 2*x
 }
-console.log(person.name)
+console.log(double(2))     //prints 4  and you can not path it more then 1 parameter
 
+function Greet (person:string = 'mina'):boolean{
+    console.log('hello',person)
+    return true
+}
+Greet();  //you can annotate the return value type
 
-//Strings
-let myName:string= 'mina isaac'
-// numbers 
-let Fsvnumber: number = 9
-//boolean
-let flag:boolean= true
-
-
-//type inference
-let car = 'mersycide'
-let favnumber = 9
-//you can't assign it to different type now
-//favnumber = 'mina'  this errors 
-
-
-
-//any type
-let color:any = 'green'
-color = 20 //no error happens
-//color();  // this will error only if you run code
